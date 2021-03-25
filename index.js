873959c5038ec90d6516f8a87692025254e6e480
Whishes <nicholas.cruickshank25@gmail.com>
2021-03-25 15:31:37 +1100

    Huge Update (Basically done)
    -> All stylings are pretty much done except for (maybe) fonts, weird font sizings...
    -> Just need to fill out the "Skills" section with actual skills
    -> Need to fill out the "Projects" section with all the required info/links
    -> Need to link everything to the correct website
    -> Need to add the email service stuff so the "Contact Me" form actually does what it needs to do

--- index.js
+++
@@ -1,5 +0,0 @@
-// Mobile viewport stuff
-window.addEventListener('resize', () => {
-    let vh = innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`); //set property to root
-});
